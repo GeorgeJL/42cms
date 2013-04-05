f47c531d74fc1b1c748d510df7d18040e2150390
GeorgeJL <lorinc.juraj@gmail.com>
2013-04-05 12:39:58 +0100

    Edited menu.php
    Now will be page level 0 displayed in menu. To hide it use "Do not show
    in menu" option in page  editor

--- includes/plugins/menu.php
+++ includes/plugins/menu.php
@@ -28,10 +28,10 @@ if(empty($pluginVars['pluginget']['dontshow']))
 
 if(empty($pluginVars['pluginget']['showpart']))
 {
-  $sql="SELECT url, id, subdomain, url_part, level, menuorder, menutitle, id, inmenu FROM ".$config->dbprefix."pages WHERE subdomain='".$subDomain."' AND active='Yes' AND inmenu!='non' AND level!='0' ".$level." ORDER BY level, menuorder, title";
+  $sql="SELECT url, id, subdomain, url_part, level, menuorder, menutitle, id, inmenu FROM ".$config->dbprefix."pages WHERE subdomain='".$subDomain."' AND active='Yes' AND inmenu!='non' ".$level." ORDER BY level, menuorder, title";
 }else{
   $showPart=$mysqli->real_escape_string($pluginVars['pluginget']['showpart']);
-  $sql="SELECT url, id, subdomain, url_part, level, menuorder, menutitle, id, inmenu FROM ".$config->dbprefix."pages WHERE subdomain='".$subDomain."' AND active='Yes' AND inmenu!='non' AND level!='0' ".$level." AND url LIKE '".$showPart."%' ORDER BY level, menuorder, title";
+  $sql="SELECT url, id, subdomain, url_part, level, menuorder, menutitle, id, inmenu FROM ".$config->dbprefix."pages WHERE subdomain='".$subDomain."' AND active='Yes' AND inmenu!='non' ".$level." AND url LIKE '".$showPart."%' ORDER BY level, menuorder, title";
 }  
 $result=$mysqli->query($sql);
 $list=array();
